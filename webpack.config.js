5fce5fd1966ca246d8ed61b6210e716be7442a06
Gaspard Etienne <gaspardetienne97@users.noreply.github.com>
2018-10-25 05:08:22 -0400

    The boilerplate is ready for development.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-module.exports = [{
-    entry: './app.scss',
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: ['./app.scss', './app.js'],
     output: {
-        // This is necessary for webpack to compile
-        // But we never use style-bundle.js
-        filename: 'style-bundle.js',
+        filename: 'bundle.js',
     },
     module: {
         rules: [
@@ -18,9 +18,28 @@ module.exports = [{
                     },
                     {loader: 'extract-loader'},
                     {loader: 'css-loader'},
-                    {loader: 'sass-loader'},
-                ]
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules'],
+                        },
+                    }
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-object-assign']
+                },
             }
-        ]
+        ],
     },
-}];
+};
