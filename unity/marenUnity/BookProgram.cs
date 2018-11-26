23b04ae0ba2512a24b3452dbef0c936a8ff6a966
maren22 <44934113+maren22@users.noreply.github.com>
2018-11-26 13:49:09 -0500

    Add files via upload

---
+++ unity/marenUnity/BookProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class BookProgram
+    {
+        static void Main1(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "J. K. Rowling", 500);
+            Console.WriteLine(book1.pages);
+
+            Book book2 = new Book("Lord Of The Rings","J. R. R. Tolkien",700);
+            Console.WriteLine(book2.pages);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ unity/marenUnity/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class StudentProgram
+    {
+        static void Main1(string[] args)
+        {
+            Student student1 = new Student("Emily","Astro",2.8);
+            Console.WriteLine(student1.gpa);
+
+            Student student2 = new Student("Max","Art",3.6);
+            Console.WriteLine(student2.gpa);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
