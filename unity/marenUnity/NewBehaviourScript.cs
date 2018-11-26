65f4f35eaea94807766ba7ff85edc3d7c37f08a0
maren22 <44934113+maren22@users.noreply.github.com>
2018-11-26 13:46:23 -0500

    Add files via upload

--- unity/marenUnity/NewBehaviourScript.cs
+++ unity/marenUnity/NewBehaviourScript.cs
@@ -1,30 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ExampleClass : MonoBehaviour
+{
+    // Creates a line renderer that follows a Sin() function
+    // and animates it.
 
-    public Color startColor;
-    public Color endColor;
-    public float StartWidth;
-    public float EndWidth;
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 20;
 
-    // Use this for initialization
-    void Start () 
+    void Start()
     {
         LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
 
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+        lineRenderer.colorGradient = gradient;
     }
 
-    // Update is called once per frame
-    void Update () 
+    void Update()
     {
         LineRenderer lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-        //lineRenderer.startWidth = StartWidth;
-        //lineRenderer.endWidth = EndWidth;
-        gameObject.GetComponent<LineRenderer>().SetWidth(StartWidth , EndWidth);
-        lineRenderer.startColor = Color.white;
-        lineRenderer.endColor = Color.white;
+        var t = Time.time;
+        for (int i = 0; i < lengthOfLineRenderer; i++)
+        {
+            lineRenderer.SetPosition(i, new Vector3(i * 0.5f, Mathf.Sin(i + t), 0.0f));
+        }
     }
 }
