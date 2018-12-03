cf27837b69f28c31d416fecca56dfac295449f87
maren22 <44934113+maren22@users.noreply.github.com>
2018-12-03 14:03:43 -0500

    Add files via upload

---
+++ unity/marenUnity/NewBehaviourScript1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public GameObject prefab;
+public float gridX = 5f;
+public float gridY = 5f;
+public float spacing = 2f;
+
+void Start()
+{
+    for (int y = 0; y < gridY; y++) 
+    {
+        for (int x = 0; x < gridX; x++)
+        {
+            Vector3 pos = new Vector3(x, 0, y) * spacing;
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+} 
+
