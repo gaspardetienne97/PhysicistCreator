c1a64a0c5e24c286daccea43712d54011ec24050
maren22 <44934113+maren22@users.noreply.github.com>
2018-12-03 14:05:52 -0500

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Color startColor;
+    public Color endColor;
+    public float StartWidth;
+    public float EndWidth;
+
+    // Use this for initialization
+    void Start () 
+    {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        //lineRenderer.startWidth = StartWidth;
+        //lineRenderer.endWidth = EndWidth;
+        gameObject.GetComponent<LineRenderer>().SetWidth(StartWidth , EndWidth);
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+    }
+}
