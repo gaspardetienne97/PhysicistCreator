48124521376804b656d06166f5e113ee368ee60c
Gaspard Etienne <gaspardetienne97@users.noreply.github.com>
2018-12-03 20:17:04 -0500

    Updates

---
+++ precache-manifest.fac21698ff09de1786a1640c82630711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/PhysicistCreator/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "efecd9d40367ec0d16517eccd2131f51",
+    "url": "/PhysicistCreator/static/media/Placeholder.efecd9d4.png"
+  },
+  {
+    "revision": "9312d5e7256896b1b41a",
+    "url": "/PhysicistCreator/static/js/runtime~main.9312d5e7.js"
+  },
+  {
+    "revision": "b2ed79ab73868a02a254",
+    "url": "/PhysicistCreator/static/js/main.b2ed79ab.chunk.js"
+  },
+  {
+    "revision": "143debc059fa2918a62d",
+    "url": "/PhysicistCreator/static/js/1.143debc0.chunk.js"
+  },
+  {
+    "revision": "b2ed79ab73868a02a254",
+    "url": "/PhysicistCreator/static/css/main.99acfacd.chunk.css"
+  },
+  {
+    "revision": "2819988e8c01587b3d275aa24bff4603",
+    "url": "/PhysicistCreator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfac650771ca7cdcc217b9e0cd7fd09e.js"
+  "/PhysicistCreator/precache-manifest.fac21698ff09de1786a1640c82630711.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PhysicistCreator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
