79452193c46fe717e37810a3601a19134745b081
maren22 <44934113+maren22@users.noreply.github.com>
2018-12-09 14:55:08 -0500

    Add files via upload

---
+++ unity/marenUnity/FieldWalker.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class FieldWalker : MonoBehaviour
+{
+
+    public BezierSpline spline;
+
+    public float duration;
+
+    private float progress;
+
+    public bool lookForward;
+
+    public SplineWalkerMode mode;
+
+    //Needed because the arrow asset we use is oritented wrong naturally.
+    private int arrowCorrection = 90;
+
+    private bool goingForward = true;
+    private void Update()
+    {
+        if (goingForward)
+        {
+            progress += Time.deltaTime / duration;
+            if (progress > 1f)
+            {
+                if (mode == SplineWalkerMode.Once)
+                {
+                    progress = 1f;
+                }
+                else if (mode == SplineWalkerMode.Loop)
+                {
+                    progress -= 1f;
+                }
+                else
+                {
+                    progress = 2f - progress;
+                    goingForward = false;
+                }
+            }
+        }
+        else
+        {
+            progress -= Time.deltaTime / duration;
+            if (progress < 0f)
+            {
+                progress = -progress;
+                goingForward = true;
+            }
+        }
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        transform.position += 15*Vector3.up;
+
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(progress));
+            transform.Rotate(0, arrowCorrection, arrowCorrection);
+        }
+    }
+}
--- unity/marenUnity/NewBehaviourScript.cs
+++ unity/marenUnity/NewBehaviourScript.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
-    public Color startColor;
-    public Color endColor;
-    public float StartWidth;
-    public float EndWidth;
+// Use this for initialization
+public GameObject prefab = new GameObject();
+public float gridX = 5f;
+public float gridY = 5f;
+public float gridZ = 5f;
+public float spacing = 2f;
 
-    // Use this for initialization
-    void Start () 
+
+void Start()
+{
+
+//Mesh myMesh = (Mesh)Resources.Load("model",typeof(Mesh)); 
+
+//prefab.AddComponent<MeshFilter>();
+//prefab.AddComponent<MeshRenderer>();
+//prefab.GetComponent<MeshFilter>().sharedMesh = myMesh;
+//prefab.GetComponent<MeshRenderer>().material.color = Color.white;
+
+    for (int y = 0; y < gridY; y++) 
     {
-        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
-
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridZ; z++)
+            {
+                Vector3 pos = new Vector3(x, z, y) * spacing;
+                Instantiate(prefab, pos, Quaternion.identity);
+                
+            }
+        }
     }
+} 
 
-    // Update is called once per frame
-    void Update () 
-    {
-        LineRenderer lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-        //lineRenderer.startWidth = StartWidth;
-        //lineRenderer.endWidth = EndWidth;
-        gameObject.GetComponent<LineRenderer>().SetWidth(StartWidth , EndWidth);
-        lineRenderer.startColor = Color.white;
-        lineRenderer.endColor = Color.white;
-    }
+	// Update is called once per frame
+	void Update () {
+	}
 }
+
+// GameObject g = GameObject.Instantiate (prefab, transform.position, prefab.transform.rotation) as GameObject;
+// g.transform.Rotate (0f, 180f, 0f);
