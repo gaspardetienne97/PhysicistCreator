a7f417b90e7f15bdcb8bd9be3bf5b6d70814c8f3
ljmcgann <33186994+ljmcgann@users.noreply.github.com>
2018-12-14 16:07:36 -0500

    Add files via upload

--- web/src/Content.js
+++ web/src/Content.js
@@ -1,8 +1,8 @@
 const content = {
     VectorSinkLoop: {
-        name: 'Math',
+        name: 'Closed path over a vector field',
         message: '  Our math module will go here it will be comprised of the visualization of a line\n' +
-            '                                        integral in 3D space',
+            '                                        integral in 3D space: curve 1',
         description: '',
         JSONBuild: "webgl/Build/910b5e962d43143a73f762a9487fdce9.json",
         UnityLoader: "webgl/Build/04cfd823429f9d23bbb0a65451607432.js",
@@ -16,7 +16,7 @@ const content = {
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     AmperesLaw: {
-        name: 'Faraday\'s Law',
+        name: 'Ampere\'s Law',
         message: 'Our physics module will build upon our math module and explain flux using line\n' +
             '                                        integrals.',
         description: '',
@@ -24,49 +24,46 @@ const content = {
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     ConstantVectorIntegralOpenLoop: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Work to move through a vector field',
+        message: 'A math module to teach the work expended to move through a vector field',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     VectorLoopConstant: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Work to move through a vector field',
+        message: 'A followup to the previous work module to illustrate concpets of conservative' +
+            '                                        vector fields.',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     VectorLoopSink: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Closed path over a vector field',
+        message: '  Our math module will go here it will be comprised of the visualization of a line\n' +
+            '                                        integral in 3D space: curve 2',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     VectorLoopDiscontinuous: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Path with a nondifferentiable point ',
+        message: 'A math module to show why nondifferentiable points do not contribute to line integral',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     VectorOpenConstant: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Work to move through a vector field',
+        message: 'A math module to teach the work expended to move through a vector field',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
     },
     ScalarOpen: {
-        name: 'Faraday\'s Law',
-        message: 'Our physics module will build upon our math module and explain flux using line\n' +
-            '                                        integrals.',
+        name: 'Line integral in a scalar field ',
+        message: 'A math module showing a geometric representation of a line integral\n' +
+            '                                        in a scalar field.',
         description: '',
         JSONBuild: "Build/Build/Build.json",
         UnityLoader: "Build/Build/UnityLoader.js",
